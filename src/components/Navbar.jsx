cad70c1638d30c6884b872da1f9ecc0942e00295
Lucca Chantre <luccachantre@gmail.com>
2025-12-18 16:27:43 -0500

    added some navbar links, set width and height of container div then did display flex and justify content space between to separate them evenly, and set the cursor to pointer when hovering over the anchor tags

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div id="nav-container">
+      <a>home</a>
+      <a>projects</a>
+      <a>about me</a>
+      <a>contact</a>
+    </div>
+  );
+};
+
+export default Navbar;
